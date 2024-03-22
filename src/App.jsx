9a92f980730590ed184a9004bbefd3aff1360e2d
kathrynhoule <kathrynhoule@torontomu.ca>
2024-03-22 18:42:44 -0400

    combines the header and navigation modules into one

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import './App.css'
 import Header from './Header';
-import Navigation from './Navigation';
 import MainContent from './MainContent'
 
 function App() {
   return (
     <div className="App">
     <Header />
-    <Navigation />
     <MainContent />
   </div>
   );
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,13 +1,28 @@
 import React from 'react';
 import './App.css';
+import instagram from './icons/instagram.png';
+import twitter from './icons/twitter.png';
+import hamburgermenu from './icons/hamburgermenu.png';
 
-function Header() {
+const Header = () => {
   return (
-    <header className ="header">
-      <div className="icon"></div>
-      <h1>Second Cards</h1>
-    </header>
+      <div className="header">
+          <div>
+          <h1>Second Cards</h1>
+          </div>
+          <nav>
+          <ul id="navbarlist">
+                   <li id="about">About</li>
+                   <li id="shop">Shop</li>
+                   <li id="cart">Cart</li>
+                   <li id="request">Request</li>
+                   <li><img src={instagram} alt="instagram" width="50" height="50" /></li>
+                   <li><img src={twitter} alt="twitter" width="45" height="45" /></li>
+               </ul>
+               <img id="hamburger-menu" src={hamburgermenu} alt="click to open menu" width="40" height="40" />
+          </nav>
+      </div>
   );
-}
+};
 
 export default Header;
