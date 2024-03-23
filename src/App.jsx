67e84aae647ece2b654d31478084c127170d6257
kathrynhoule <kathrynhoule@torontomu.ca>
2024-03-22 20:02:16 -0400

    moves image gallery to its own module

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import Header from './Header';
-import MainContent from './MainContent'
+import MainContent from './MainContent';
+import ProductGallery from './ProductGallery';
 
 function App() {
   return (
     <div className="App">
     <Header />
     <MainContent />
+    <ProductGallery />
   </div>
   );
 }
--- src/MainContent.jsx
+++ src/MainContent.jsx
@@ -1,13 +1,5 @@
 import React from 'react';
 import arrow from './icons/arrow.png';
-import ghostrickalucard from './cards/ghostrickalucard.png';
-import ghostrickfestival from './cards/ghostrickfestival.png';
-import illumi from './cards/illumi.png';
-import machi from './cards/machi.png';
-import milotic from './cards/milotic.png';
-import rayquaza from './cards/rayquaza.png';
-import rotom from './cards/rotom.png';
-import shaymin from './cards/shaymin.png';
 
 function MainContent() {
   return <main>
@@ -29,45 +21,6 @@ function MainContent() {
                <li>Rarity</li>
                <li>Release Date</li>
            </ul>
-
-           <div className="container">
-               <div className="grid">
-                 <div className="cell">
-                   <img src={shaymin} alt="card 1" className="responsive-image" />
-                   <p>Example Card 1</p>
-                 </div>
-                 <div className="cell">
-                   <img src={milotic} alt="card 2" className="responsive-image" />
-                   <p>Example Card 2</p>
-                 </div>
-                 <div className="cell">
-                   <img src={rotom} alt="card 3" cclassName="responsive-image" />
-                   <p>Example Card 3</p>
-                 </div>
-                 <div className="cell">
-                    <img src={ghostrickalucard} alt="card 4" className="responsive-image" />
-                    <p>Example Card 4</p>
-                  </div>
-                  <div className="cell">
-                    <img src={illumi} alt="card 5" className="responsive-image" />
-                    <p>Example Card 5</p>
-                  </div>
-                  <div className="cell">
-                    <img src={machi} alt="card 6" className="responsive-image" />
-                    <p>Example Card 6</p>
-                  </div>
-                  <div className="cell">
-                    <img src={rayquaza} alt="card 7" className="responsive-image" />
-                    <p>Example Card 7</p>
-                  </div>
-                  <div className="cell">
-                    <img src={ghostrickfestival} alt="card 8" className="responsive-image" />
-                    <p>Example Card 8</p>
-                  </div>
-               </div>
-             </div>
-
-
   </main>;
 }
 
