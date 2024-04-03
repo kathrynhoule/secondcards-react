0a357c81e3bbb22f50c70ee6a645207cc2251f93
kathrynhoule <kathrynhoule@torontomu.ca>
2024-04-03 19:08:41 -0400

    adds header to each page

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,1 +1,11 @@
-//import Header from '../components/Header';
+import Header from '../components/Header';
+
+function Cart() {
+     return (
+          <div>
+<Header />
+</div>
+     );
+}
+   
+export default Cart;
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,1 +1,11 @@
-//import Header from '../components/Header';
+import Header from '../components/Header';
+
+function Contact() {
+     return (
+          <div>
+<Header />
+</div>
+     );
+}
+   
+export default Contact;
