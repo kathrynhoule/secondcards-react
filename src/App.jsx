0654078d0d5ef70f375f0ec93abee02050731abb
kathrynhoule <kathrynhoule@torontomu.ca>
2024-04-03 17:38:54 -0400

    moves items from one file to another

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 //import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css'
 import Header from './components/Header';
-import MainContent from './MainContent';
+import Filter from './pages/Shop';
 import ProductGallery from './ProductGallery';
 
 function App() {
@@ -10,7 +10,7 @@ function App() {
     //<Router>
       <div className="App">
         <Header />
-        <MainContent />
+        <Filter />
         <ProductGallery />
       </div>
   //</Router>
---
+++ src/pages/Shop.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import arrow from '../icons/arrow.png';
+
+function Filter() {
+     return <main>
+        <ul id="sublist">
+                  <li id="singles"><a href="">Singles</a></li>
+                  <li id="packs"><a href="">Packs</a></li>
+                  <li id="boxes"><a href="">Boxes</a></li>
+                  <li id="all"><a href="">All</a></li>
+              </ul>
+              <div class="dropdown"><p>Filter by</p> <img id="arrow" src={arrow} alt="click to open menu" width="20" height="20" />
+              </div>
+              <div class="search"><p>search...</p></div>
+             
+              <hr />
+   
+              <ul id="lastlist">
+                  <li>Filter by:</li>
+                  <li>Series</li>
+                  <li>Rarity</li>
+                  <li>Release Date</li>
+              </ul>
+     </main>;
+   }
+   
+   export default Filter;
