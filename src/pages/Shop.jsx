e62802eeea0f6d11efffad34b0bb5c414459ca66
kathrynhoule <kathrynhoule@torontomu.ca>
2024-04-04 13:19:53 -0400

    adds more styling to the search bar

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 import Filter from '../components/Filter';
 import ProductGallery from '../components/ProductGallery';
 import SearchBar from '../components/SearchBar';
+import '../components/SearchBar.css';
 
 function Shop() {
      return (
           <div>
+               <div className = "searchbarcontainer">
                <SearchBar />
+               </div>
                <Filter />
                <ProductGallery />
           </div>
