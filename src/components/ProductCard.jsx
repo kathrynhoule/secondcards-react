9049c1c33fdfc8be2f9e83897b9fac6718801adf
kathrynhoule <kathrynhoule@torontomu.ca>
2024-04-09 16:06:56 -0400

    adds additional styling to the product cards

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -6,11 +6,9 @@ const ProductCard = ({product}) => {
      <div className = "productcard">
           <img src={product.image} alt={product.name} />
           <h2>{product.name}</h2>
-          <p>Category: {product.category}</p>
           <p>Product Type: {product.producttype}</p>
-          <p>Game: {product.game}</p>
           <p>Rarity: {product.rarity}</p>
-          <p>Price: ${product.price}</p>
+          <p className="productprice">Price: ${product.price}</p>
      </div>
 );
 }
