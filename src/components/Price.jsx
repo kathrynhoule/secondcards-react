db6816e6980e540390a35e6b76e9375357de346a
kathrynhoule <kathrynhoule@torontomu.ca>
2024-04-15 13:14:06 -0400

    begins adding in price filter component WIP

--- src/components/Price.jsx
+++ src/components/Price.jsx
@@ -1,9 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Checkbox from './Checkbox';
 
-function Price() {
+function Price({ onPriceSelectionChange }) {
+
+     const [selectedPrices, setSelectedPrices] = useState([]);
+
+     const handleOptionChange = (event) => {
+          const value = event.target.value;
+          setSelectedPrices(current => {
+               const newSelection = current.includes(value) ? current.filter(item => item !== value) : [...current, value];
+               onPriceSelectionChange(newSelection);
+               return newSelection;
+          });
+     };
+
+     const options = [
+          { value: '0-10', label: '$0 - $10' },
+          { value: '10-20', label: '$10 - $20' },
+          { value: '20-30', label: '$20 - $30' },
+          { value: '30-40', label: '$30 - $40' },
+          { value: '40-50', label: '$40 - $50' },
+          { value: '50+', label: '$50+' },
+     ];
+
      return (
           <div>
                <h3>PRICE</h3>
+               <Checkbox
+                    name="priceOptions"
+                    options={options}
+                    selectedOptions={selectedPrices}
+                    onChange={handleOptionChange}
+               />
           </div>
         );
 }
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './Sidebar.css';
 import Game from './Game';
 import Type from './Type';
-// import Price from './Price';
+import Price from './Price';
 // import Rarity from './Rarity';
 // import ReleaseDate from './ReleaseDate';
 
@@ -11,6 +11,7 @@ function Sidebar({ onGameFilterChange,  onTypeFilterChange}) {
           <section className="sidebar">
                <Game onGameSelectionChange={onGameFilterChange} />
                <Type onTypeSelectionChange={onTypeFilterChange} />
+               <Price />
           </section>
      );
 }
