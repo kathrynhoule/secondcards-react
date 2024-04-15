b0299639dce34b9810a46613dc126e022a55a549
kathrynhoule <kathrynhoule@torontomu.ca>
2024-04-15 10:37:03 -0400

    finishes implementing the filters for games, adjusts some product data

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,21 +1,17 @@
 import React from 'react';
 import './Sidebar.css';
 import Game from './Game';
-import Category from './Category';
-import Price from './Price';
-import Rarity from './Rarity';
-import ReleaseDate from './ReleaseDate';
+// import Category from './Category';
+// import Price from './Price';
+// import Rarity from './Rarity';
+// import ReleaseDate from './ReleaseDate';
 
-function Sidebar() {
+function Sidebar({ onGameFilterChange }) {
      return (
           <section className="sidebar">
-               <Game />
-               <Category />
-               <Price />
-               <Rarity />
-               <ReleaseDate />
+               <Game onGameSelectionChange={onGameFilterChange} />
           </section>
-        );
+     );
 }
    
 export default Sidebar;
