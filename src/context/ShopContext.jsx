34d5ae95061ab070e28d2ab542c6f6eb6c3291ab
kathrynhoule <kathrynhoule@torontomu.ca>
2024-04-18 13:37:42 -0400

    adds new folder and basic file for context

---
+++ src/context/ShopContext.jsx
@@ -0,0 +1,10 @@
+import React, { createContext } from 'react';
+
+export const ShopContext = createContext(null);
+
+export const ShopContextProvider = () => {
+     return
+     <div>
+     </div>;
+};
+
