4c2a7ae4ec65490f330b29540df403663c3a379c
kathrynhoule <kathrynhoule@torontomu.ca>
2024-04-18 13:46:33 -0400

    adds default state for cart

--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -1,10 +1,21 @@
-import React, { createContext } from 'react';
+import React, { createContext, useState } from 'react';
+import products from '../data/ProductData';
 
 export const ShopContext = createContext(null);
 
-export const ShopContextProvider = () => {
+const getDefaultCart = () => {
+     let cart = {}
+     for (let i = 1; i < products.length + 1; i++) {
+          cart[i] = 0;
+     }
+     return cart;
+};
+
+export const ShopContextProvider = (props) => {
+     const [cartItems, setCartItems] = useState();
      return
-     <div>
-     </div>;
+          <ShopContext.Provider>
+               {props.children}
+          </ShopContext.Provider>;
 };
 
