3ca3c0e837ced961b00f12bfd728fe12f7dd58d9
kathrynhoule <kathrynhoule@torontomu.ca>
2024-04-19 16:01:28 -0400

    adds basic text to ordersummary component

--- src/components/OrderSummary.jsx
+++ src/components/OrderSummary.jsx
@@ -4,6 +4,13 @@ import './OrderSummary.css';
 function OrderSummary() {
      return (
           <div className="ordersummary">
+               <h1>Order Summary</h1>
+               <h3>Edit Cart</h3>
+               <h3>Items:</h3>
+               <h3>Shipping & Handling:</h3>
+               <h3>Taxes:</h3>
+
+               <h2>Order Total:</h2>
           </div>
         );
 }
