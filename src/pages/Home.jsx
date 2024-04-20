c69cf95258f085afd8d27736ee545c778f2c27db
kathrynhoule <kathrynhoule@torontomu.ca>
2024-04-20 16:10:44 -0400

    adds basic flex column formatting to the homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
+import './Home.css';
+
 function Home() {
      return (
-          <div>
-</div>
+          <div className="home">
+               <div className="container">
+                    <div className="column column1">Column 1</div>
+                    <div className="column column2">Column 2</div>
+                    <div className="column column3">Column 3</div>
+               </div>
+          </div>
      );
 }
    
